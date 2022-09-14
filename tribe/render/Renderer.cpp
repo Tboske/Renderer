e855aa0ae94dfdf6e3ebce2f8b63a49bab949663
Tboske <jeffke.de.meutter@gmail.com>
2022-09-15 01:32:37 +0200

    applied pImpl idiom to renderclass

--- tribe/render/Renderer.cpp
+++ tribe/render/Renderer.cpp
@@ -1,51 +1,77 @@
 #include "Renderer.h"
 
-#include "GLFW/glfw3.h"
+#include <GLFW/glfw3.h>
 #include <backends/imgui_impl_opengl3.h>
 #include <backends/imgui_impl_glfw.h>
 
-
-Renderer::Renderer()
-{    
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    m_pWindow = glfwCreateWindow(1280, 720, "Testing Window", NULL, NULL);
-    if (!m_pWindow)
-        throw "Failed to initialize window";        
-
-    glfwMakeContextCurrent(m_pWindow);
-    glfwSwapInterval(1); // enable vsync
-
-
-    InitImGui();
-}
-
-Renderer::~Renderer()
+class Renderer::RendererImpl
 {
-    glfwDestroyWindow(m_pWindow);
+public:
+    RendererImpl(Renderer* pBaseRenderer)
+        : m_pBaseRenderer(pBaseRenderer)
+    {
+    }
+    virtual ~RendererImpl()
+    {
+        m_pBaseRenderer = nullptr;
+    }
+    RendererImpl(const RendererImpl&) = delete;
+    RendererImpl(RendererImpl&&) noexcept  = delete;
+    RendererImpl& operator=(const RendererImpl&) = delete;
+    RendererImpl& operator=(RendererImpl&&) noexcept  = delete;    
 
-    DestroyImGui();
-}
+    virtual void Draw() = 0;
+    virtual void InitImGui() = 0;
+    virtual void DestroyImGui() = 0;
+    virtual void RenderImGui() = 0;
 
-void Renderer::Update()
+protected:
+    Renderer* m_pBaseRenderer;
+};
+
+#pragma region RendererOpengl3
+class RendererOpengl3 final : public Renderer::RendererImpl
+{
+public:
+    RendererOpengl3(Renderer* pBaseRenderer)
+        : Renderer::RendererImpl(pBaseRenderer)
+    {
+    }
+    virtual ~RendererOpengl3() override = default;
+    RendererOpengl3(const RendererOpengl3&) = delete;
+    RendererOpengl3(RendererOpengl3&&) noexcept  = delete;
+    RendererOpengl3& operator=(const RendererOpengl3&) = delete;
+    RendererOpengl3& operator=(RendererOpengl3&&) noexcept  = delete;
+    
+    virtual void Draw() override;
+
+    virtual void InitImGui() override;
+    virtual void DestroyImGui() override;
+    virtual void RenderImGui() override;
+
+private:
+
+};
+
+void RendererOpengl3::Draw()
 {
     // Rendering
     int display_w, display_h;
-    glfwGetFramebufferSize(m_pWindow, &display_w, &display_h);
+    glfwGetFramebufferSize(m_pBaseRenderer->m_pWindow, &display_w, &display_h);
     glViewport(0, 0, display_w, display_h);
 
-    glClearColor(m_ClearColor.x * m_ClearColor.w, m_ClearColor.y * m_ClearColor.w, m_ClearColor.z * m_ClearColor.w, m_ClearColor.w);
+    const auto& color = m_pBaseRenderer->m_ClearColor;
+    glClearColor(color.x * color.w, color.y * color.w, color.z * color.w, color.w);
     glClear(GL_COLOR_BUFFER_BIT);
 
     // render everything from the scenegraph
     RenderImGui();
 
-    glfwSwapBuffers(m_pWindow);
+    glfwSwapBuffers(m_pBaseRenderer->m_pWindow);
     glfwPollEvents();
 }
 
-
-void Renderer::InitImGui()
+void RendererOpengl3::InitImGui()
 {
     // setup imgui
     const char* glsl_version = "#version 130";
@@ -71,11 +97,11 @@ void Renderer::InitImGui()
     }
 
     // Setup Platform/Renderer backends
-    ImGui_ImplGlfw_InitForOpenGL(m_pWindow, true);
+    ImGui_ImplGlfw_InitForOpenGL(m_pBaseRenderer->m_pWindow, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
 }
 
-void Renderer::DestroyImGui()
+void RendererOpengl3::DestroyImGui()
 {
     // Cleanup
     ImGui_ImplOpenGL3_Shutdown();
@@ -83,7 +109,7 @@ void Renderer::DestroyImGui()
     ImGui::DestroyContext();
 }
 
-void Renderer::RenderImGui()
+void RendererOpengl3::RenderImGui()
 {
     // Start the Dear ImGui frame
     ImGui_ImplOpenGL3_NewFrame();
@@ -105,3 +131,37 @@ void Renderer::RenderImGui()
         glfwMakeContextCurrent(backup_current_context);
     }
 }
+#pragma endregion
+
+
+#pragma region Renderer
+Renderer::Renderer()
+{
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    m_pWindow = glfwCreateWindow(1280, 720, "Testing Window", NULL, NULL);
+    if (!m_pWindow)
+        throw "Failed to initialize window";        
+
+    glfwMakeContextCurrent(m_pWindow);
+    glfwSwapInterval(1); // enable vsync
+
+    m_pImpl = new RendererOpengl3(this);
+
+    m_pImpl->InitImGui();
+}
+
+Renderer::~Renderer()
+{
+    m_pImpl->DestroyImGui();
+
+    SafeDelete(m_pImpl);
+
+    glfwDestroyWindow(m_pWindow);
+}
+
+void Renderer::Draw()
+{
+    m_pImpl->Draw();
+}
+#pragma endregion
